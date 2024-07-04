b4d45ad6b6548c7cded7922f66bfa2578b15eefb
Alhafiz63 <hafizbanjar.jan05@gmail.com>
2024-07-04 15:28:35 +0800

    fix: from banjarmasin

--- tes.cpp
+++ tes.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, i'm from poliban" << std::endl;
+    std::cout << "Hello, i'm from banjarmasin" << std::endl;
     return 0;
 }
